e185de879b6645cae6bdb8eb077038ed57b4a2c6
Saurabh Mehta <spmehta93@gmail.com>
2024-06-14 08:03:36 +0530

    Added new file

---
+++ 06_14Jun2026/Lab052.py
@@ -0,0 +1,9 @@
+# pass some information to the function
+
+def greet(name):
+    print(f"Hello {name}")
+
+greet("Saurabh")
+greet("Pramod")
+greet("213")
+greet(123)
---
+++ 06_14Jun2026/Lab053.py
@@ -0,0 +1,9 @@
+
+def allowed_to_attend_python_class(name,password):
+    if name=="saurabh" and password=="Yes":
+        print("Allowed")
+    else:
+        print("Not allowed")
+
+allowed_to_attend_python_class("saurabh","Yes")
+allowed_to_attend_python_class("pramod","Yes")
